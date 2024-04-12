f135d794976cdf2b8fc1a70112ad7f6b2d2ec23b
luka-danger <lukathevegan@gmail.com>
2024-04-12 13:53:25 -0600

    Rebuild program logic
    Build create player factory,
    Create empty Gameboard IIFE,
    Create Game IFFE,
    Create startGame module,
    Add checkForWin function,
    Add winning combos array inside of checkForWin

--- app.js
+++ app.js
@@ -1,90 +1,40 @@
-// Gameboard Factory Function / IIFE
+// New Code
 
-const displayController = (() => {
-    const displayMessage = (message) => {
-        document.querySelector('#message').innerHTML = message;
-    }
-    return {
-        displayMessage
-    }
-})();
+let gameBoard; 
+const player1 = 'X';
+const computerPlayer = 'O';
 
+
+// Gameboard IIFE
 const Gameboard = (() => {
-    // Empty array with length of 9 
-    let gameboard = ["", "", "", "", "", "", "", "", ""];
-    
-    const displayBoard = () => {
-        let boardHTML = "";
-        gameboard.forEach((square, index) => {
-            boardHTML += `<div class="square" id="square-${index}">${square}</div>`
-        })
-        document.querySelector('#gameboard').innerHTML = boardHTML;
-        const squares = document.querySelectorAll('.square');
-        squares.forEach((square) => {
-            square.addEventListener('click', Game.handleClick);
-        })
-    }
 
-    const updateBoard = (index, value) => {
-        gameboard[index] = value;
-        displayBoard();
-    }
-
-    const getGameboard = () => gameboard;
-
-    return {
-        displayBoard,
-        updateBoard,
-        getGameboard
-    }
 })();
 
-const createPlayer = (playerName, symbol) => {
+// Create Player Factory
+const createPlayer = (playerName, playerSymbol) => {
     return {
         playerName,
-        symbol
+        playerSymbol
     }
 }
 
+// Game IIFE
 const Game = (() => {
     let players = [];
-    let currentPlayerIndex;
-    let gameOver;
 
     const startGame = () => {
         players = [
-            createPlayer(document.querySelector("#player1").value, "X"),
-            createPlayer(document.querySelector("#player2").value, "O")
+            createPlayer(document.querySelector('#player1').value, 'X'),
+            createPlayer(computerPlayer, 'O')
         ]
-        currentPlayerIndex = 0;
-        gameOver = false;
-        Gameboard.displayBoard();
-        
-    }
 
-    const handleClick = (event) => {
-        // Split separates words ex: "Square", 1
-        // instead of "s", "q", "u"...etc
-        // Return [1] to only return index number
-        let index = event.target.id.split("-")[1];
 
-        // Prevent non-empty squares from being changed
-        if (Gameboard.getGameboard()[index] !== "") {
-            return 
-        }
-        Gameboard.updateBoard(index, players[currentPlayerIndex].symbol);
-
-        if (checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].symbol)) {
-            gameOver = true;
-            displayController.displayMessage(`${players[currentPlayerIndex].playerName} wins!`)
-        }
-        else if (checkForTie(Gameboard.getGameboard())){
-            gameOver = true;
-            displayController.displayMessage("It's a tie")
-        }
-        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
-    }
-
+        const squares = document.querySelectorAll('.square');
+        squares.forEach((square) => {
+            square.addEventListener('click', handleClick)
+        })
+    } 
+    
     function checkForWin(board) {
         const winningCombo = [
             [0, 1, 2],
@@ -104,35 +54,7 @@ const Game = (() => {
         }
         return false;
     }
-
-    function checkForTie(board) {
-        return board.every(cell => cell !== "")
-    }
-
-    const restart = () => {
-        for (let i = 0; i < 9; i++) { 
-            Gameboard.updateBoard(i, "");
-        }
-        Gameboard.displayBoard();
-        document.querySelector('#message').innerText = "";
-    }
-
-    return {
-        startGame,
-        restart,
-        handleClick
-    }
 })();
-
-const startButton = document.querySelector('#start-button');
-startButton.addEventListener('click', () => {
-    Game.startGame();
-})
-
-const restartButton = document.querySelector('#restart-button');
-restartButton.addEventListener('click', () => {
-    Game.restart();
-})
 
 
 
