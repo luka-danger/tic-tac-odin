06e05f8681b705c5d2fece635e6a6c32270e79e0
luka-danger <lukathevegan@gmail.com>
2024-04-12 18:01:22 -0600

    Create new functions

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const createPlayer = (playerName, playerSymbol) => {
 
 // Game IIFE
 const Game = (() => {
-
 })();
 
 let gameboard; 
@@ -34,7 +33,6 @@ function startGame() {
     document.querySelector('.endgame').style.display = "none";
 
     gameboard = Array.from(Array(9).keys());
-    console.log(gameboard);
     // change to a forEach
     squares.forEach((square) => {
         square.innerText = '';
@@ -42,20 +40,75 @@ function startGame() {
         square.addEventListener('click', handleClick)
     })
 }
+startGame();
 
 function handleClick(square) {
-    console.log(`Square ${square.target.id} clicked.`);
-    playerTurn(square.target.id, player1)
+    // If TypeOf Id is a number, it means that nobody has played in square
+    if (typeof gameboard[square.target.id] =='number') {
+        playerTurn(square.target.id, player1);
+    }
+    if (!checkTie()) turn(bestSpot(), computer);
 }
 
 function playerTurn(squareID, player){
     gameboard[squareID] = player;
     document.getElementById(squareID).innerHTML = player;
+    let gameWon = checkForWin(gameboard)
+    if (gameWon) {
+        gameOver(gameWon) 
+    } 
 }
 
-const startButton = document.querySelector('#start-button');
-startButton.addEventListener('click', () => {
-    let playerName = document.querySelector('#player1').value;
-    console.log(`${playerName} is ready to play`)
+function checkForWin(board) {
+    const winningCombo = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    for (let i = 0; i < winningCombo.length; i++) {
+        const [a, b, c] = winningCombo[i];
+        if (board[a] && board[a] === board[b] &&  board[a] === board[c]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function gameOver() {
+    squares.forEach((square) => {
+        square.removeEventListener('click', handleClick)
+    })
+}
+
+
+function emptySquares() {
+    return gameboard.filter(s => typeof s == 'number')
+}
+
+// Play in first emptySquare
+function bestSpot() {
+    return emptySquares()[0];
+}
+
+function checkTie() {
+    if (emptySquares().length == 0) {
+        squares.forEach((square) => {
+            square.backgroundColor = 'green'
+        })
+        declareWinner("Tie Game");
+        return true;
+    }
+    return false;
+}
+
+const replayButton = document.querySelector('#replay-button');
+replayButton.addEventListener('click', () => {
     startGame();
 })
+
+
